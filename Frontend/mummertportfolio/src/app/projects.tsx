6a1e088ffccee74a36761a87f1bb3775cb14742f
MummertAR <135379751+ARMummert@users.noreply.github.com>
2024-09-11 14:26:35 -0700

    updates to projects

--- Frontend/mummertportfolio/src/app/projects.tsx
+++ Frontend/mummertportfolio/src/app/projects.tsx
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
+
 interface Project {
   id: number;
   name: string;
@@ -14,6 +15,7 @@ interface Project {
   image_url: string;
   demo_url: string;
   github_url: string;
+  built_with: string;
 }
 
 const formatUrl = (url: string) => {
@@ -50,9 +52,10 @@ const ProjectComponent: React.FC = () => {
         id: 1,
         name: 'Temperature Conversion',
         description: 'A temperature conversion tool that converts Fahrenheit to Celsius and vice versa.',
-        image_url: 'https://www.github.com',
+        image_url: '/images/project1.png',
         demo_url: 'https://example.com/demo1',
         github_url: 'https://github.com/ARMummert/temp_conversion',
+        built_with: 'npm NODEJS'
       },
       {
         id: 2,
@@ -61,19 +64,51 @@ const ProjectComponent: React.FC = () => {
         image_url: 'https://www.github.com',
         demo_url: 'https://example.com/demo2',
         github_url: 'https://github.com/user/project2',
+        built_with: 'npm NODEJS'
+      },
+      {
+        id: 3,
+        name: 'Project Three',
+        description: 'This is a description for Project Two.',
+        image_url: 'https://www.github.com',
+        demo_url: 'https://example.com/demo2',
+        github_url: 'https://github.com/user/project2',
+        built_with: 'npm NODEJS'
+      },
+      {
+        id: 3,
+        name: 'Project Three',
+        description: 'This is a description for Project Two.',
+        image_url: 'https://www.github.com',
+        demo_url: 'https://example.com/demo2',
+        github_url: 'https://github.com/user/project2',
+        built_with: 'npm NODEJS'
       },
     ];
   
     setSampleProjects(sampleProjects);
   }, []);
+  
+  const projectContainerRef = useRef<HTMLDivElement>(null);
+
+  const scroll = (scrollOffset: number) => {
+  projectContainerRef.current?.scrollBy({ left: scrollOffset, behavior: 'smooth' });
+  };
+
 
   return (
+    <div className={styles.projectsSection}>
+    <div className={styles.scrollButton}>
+      <button onClick={() => scroll(-300)} className={styles.scrollButton}>Back</button>
+      <button onClick={() => scroll(300)} className={styles.scrollButton}>Forward</button> 
+   
     <div className={styles.projectContainer}>
       <div className={styles.projectWrapper}>
         {sampleProjects.map((project) => (
       <div className={styles.singleproject} key={project.id}>
-      <h3>{project.name}</h3>
-      <p>{project.description}</p>
+        <Image src={project.image_url} className="img" alt="projectimg" width={400} height={300}/>
+      <h3 className={styles.projectname}>{project.name}</h3>
+      <p className={styles.projDesc}>{project.description}</p>
 
       <div className={styles.buttonContainer}>
         <a href={formatUrl(project.demo_url)} target="_blank" rel="noopener noreferrer">
@@ -83,8 +118,12 @@ const ProjectComponent: React.FC = () => {
           <FontAwesomeIcon className={styles.icon} icon={faGithub} />
         </a>
       </div>
+      <div><p className={styles.built}>{project.built_with}</p></div>
     </div>
   ))}
+  <button onClick={() => scroll(300)} className={styles.scrollButton}>Forward</button> 
+    </div>
+   </div>
 </div>
     <div className={styles.projectWrapper}>
       {projects.map((project) => (
@@ -94,11 +133,11 @@ const ProjectComponent: React.FC = () => {
          
           <div className={styles.buttonContainer}>
           <a href={formatUrl(project.demo_url)} target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon className={styles.icon4} icon={faGithub} />
-              </a>
-              <a href={formatUrl(project.github_url)} target="_blank" rel="noopener noreferrer">
-                <button className={styles.githubButton}>GitHub</button>
-              </a>
+          <FontAwesomeIcon className={styles.icon} icon={faGithub} />
+          </a>
+          <a href={formatUrl(project.github_url)} target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon className={styles.icon} icon={faGithub} />
+          </a>
           </div>
         </div>
 
